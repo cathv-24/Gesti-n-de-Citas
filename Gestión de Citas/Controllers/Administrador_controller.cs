40b86b7cb26435fee184142bcf24ab0544a92352
cath <catherinva1124@gmail.com>
2026-05-12 00:17:20 -0500

    creación de entities y controllers
    entites y controllers 1.0

---
+++ Gestión de Citas/Controllers/Administrador_controller.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gestión_de_Citas.Controllers
+{
+    internal class administrador_controller
+    {
+    }
+}
